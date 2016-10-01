46d301ef0f2c98031b8920560ce516dd33b1c147
butterniks <yuy104@ucsd.edu>
2016-09-30 21:39:30 -0700

    asdf

---
+++ temp.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	console.log('asdf');
+	document.getElementById("button").addEventListener("click", function(){
+		console.log("asdf");
+		console.log(document.getElementById("textBox"));
+
+	});
+}
+
